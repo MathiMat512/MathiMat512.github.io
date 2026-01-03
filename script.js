cd7fa984ba0b6b347883b0c365d29a69beaba4ae
MathiMat512 <mathias.mathimat12@gmail.com>
2026-01-03 13:32:25 -0500

    Se añadió un nuevo proyecto y mejorar algunas cosas

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*CARRUSEL 1 */
 const images1 = document.querySelectorAll('.carousel-section')[0].querySelector('.carousel-images');
 const prevButton1 = document.getElementById('prev');
 const nextButton1 = document.getElementById('next');
@@ -18,6 +19,7 @@ nextButton1.addEventListener('click', () => {
   updateCarousel1();
 });
 
+/*CARRUSEL 2 */
 const images2 = document.querySelectorAll('.carousel-section')[1].querySelector('.carousel-images');
 const prevButton2 = document.getElementById('prev1');
 const nextButton2 = document.getElementById('next1');
@@ -36,4 +38,25 @@ prevButton2.addEventListener('click', () => {
 nextButton2.addEventListener('click', () => {
   currentIndex2 = (currentIndex2 < totalImages2 - 1) ? currentIndex2 + 1 : 0;
   updateCarousel2();
+});
+
+/* CARRUSEL 3 */
+const images3 = document.querySelectorAll('.carousel-section')[2].querySelector('.carousel-images');
+const prevButton3 = document.getElementById('prev2');
+const nextButton3 = document.getElementById('next2');
+const totalImages3 = images3.querySelectorAll('img').length;
+let currentIndex3 = 0;
+
+function updateCarousel3() {
+  images3.style.transform = `translateX(-${currentIndex3 * 100}%)`;
+}
+
+prevButton3.addEventListener('click', () => {
+  currentIndex3 = (currentIndex3 > 0) ? currentIndex3 - 1 : totalImages3 - 1;
+  updateCarousel3();
+});
+
+nextButton3.addEventListener('click', () => {
+  currentIndex3 = (currentIndex3 < totalImages3 - 1) ? currentIndex3 + 1 : 0;
+  updateCarousel3();
 });
